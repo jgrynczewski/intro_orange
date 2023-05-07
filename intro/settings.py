c240e729d0b40f7b6ec9c30f9fe600abf5e11f0e
jgrynczewski <jgrynczewski@gmail.com>
2023-05-07 13:52:13 +0200

    django orm - ddl

--- intro/settings.py
+++ intro/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3-rd part apps
+    'django_extensions',
+
     # local apps
     'helo.apps.HeloConfig',
     'next_app.apps.NextAppConfig',
@@ -48,6 +51,7 @@ INSTALLED_APPS = [
     'form_app2.apps.FormApp2Config',
     'form_app3.apps.FormApp3Config',
     'form_app4.apps.FormApp4Config',
+    'form_app5.apps.FormApp5Config',
 ]
 
 MIDDLEWARE = [
