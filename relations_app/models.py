e6da022068b748331157f5d6795a4ea73a06e577
jgrynczewski <jgrynczewski@gmail.com>
2023-05-08 21:07:26 +0200

    o2m

--- relations_app/models.py
+++ relations_app/models.py
@@ -9,3 +9,14 @@ class Country(models.Model):
 
 class Capital(models.Model):
     name = models.CharField(max_length=100)
+
+
+# One-to-many (O2M)
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+    # framework_set =  # automatycznie dorobiony przez django menadżer powiązany
+
+class Framework(models.Model):
+    name = models.CharField(max_length=100)
+    language = models.ForeignKey('Language', on_delete=models.CASCADE)
+    # language_id = ...  # automatycznie dorobione przez django pole
