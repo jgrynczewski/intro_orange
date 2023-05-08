fac3aad190888dced48a82502c8bd22c892688b6
jgrynczewski <jgrynczewski@gmail.com>
2023-05-08 20:41:29 +0200

    o2o

--- relations_app/models.py
+++ relations_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+# One-to-one (O2O)
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    capital = models.OneToOneField('Capital', on_delete=models.CASCADE)
+
+
+class Capital(models.Model):
+    name = models.CharField(max_length=100)
