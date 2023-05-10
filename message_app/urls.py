24f7dd8053bc29efa90ef08b67a342ddade844f2
jgrynczewski <jgrynczewski@gmail.com>
2023-05-10 20:43:36 +0200

    html form

--- message_app/urls.py
+++ message_app/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from django.urls import path
 
+from message_app import views
+
+app_name = 'message_app'
+
+urlpatterns = [
+    path('contact1/', views.contact1, name='contact1')
 ]
--- message_app/views.py
+++ message_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from message_app.models import Message
+
+
+# html form
+def contact1(request):
+    if request.method == "POST":
+        data = request.POST
+        email = data.get('email')
+        category = data.get('category')
+        subject = data.get('subject')
+        body = data.get('body')
+        fav_date = data.get('fav_date')
+        fav_time = data.get('fav_time')
+
+        Message.objects.create(
+            email=email,
+            category=category,
+            subject=subject,
+            body=body,
+            fav_date=fav_date,
+            fav_time=fav_time,
+        )
+        return redirect('message_app:contact1')
+
+    return render(
+        request,
+        'message_app/contact1.html',
+    )
