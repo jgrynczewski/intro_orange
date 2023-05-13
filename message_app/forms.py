69c9816ed3d1e074c3f523a1b46a209047f8c578
jgrynczewski <jgrynczewski@gmail.com>
2023-05-13 10:51:09 +0200

    django form

--- message_app/forms.py
+++ message_app/forms.py
@@ -10,6 +10,15 @@ class MessageForm(forms.Form):
     email = forms.EmailField(label='Email')
     category = forms.ChoiceField(choices=CHOICES, label="Kategoria")
     subject = forms.CharField(label="Tytuł")
-    body = forms.CharField(label="Treść")
-    fav_date = forms.DateField(label="Ulubiona data")
-    fav_time = forms.TimeField(label="Ulubiony czas")
+    body = forms.CharField(
+        label="Treść",
+        widget=forms.Textarea(attrs={'cols': 40, 'rows': 10})
+    )
+    fav_date = forms.DateField(
+        label="Ulubiona data",
+        widget=forms.NumberInput(attrs={'type': 'date'})
+    )
+    fav_time = forms.TimeField(
+        label="Ulubiony godzina",
+        widget=forms.NumberInput(attrs={'type': 'time'})
+    )
--- message_app/views.py
+++ message_app/views.py
@@ -15,6 +15,8 @@ def contact1(request):
         fav_date = data.get('fav_date')
         fav_time = data.get('fav_time')
 
+        # walidacja na backendzie (my tutaj przepuszczamy wszystko)
+
         Message.objects.create(
             email=email,
             category=category,
@@ -31,13 +33,46 @@ def contact1(request):
     )
 
 
+# django form
 def contact2(request):
-    form = MessageForm()
+    if request.method == "POST":
+        form = MessageForm(request.POST)  # bound form
 
-    return render(
-        request,
-        'message_app/contact2.html',
-        context={
-            'form': form,
-        }
-    )
+        if form.is_valid():
+            data = form.cleaned_data
+            email = data.get('email')
+            category = data.get('category')
+            subject = data.get('subject')
+            body = data.get('body')
+            fav_date = data.get('fav_date')
+            fav_time = data.get('fav_time')
+
+            Message.objects.create(
+                email=email,
+                category=category,
+                subject=subject,
+                body=body,
+                fav_date=fav_date,
+                fav_time=fav_time,
+            )
+
+            return redirect('message_app:contact2')
+        else:
+            return render(
+                request,
+                'message_app/contact2.html',
+                context={
+                    'form': form,
+                }
+            )
+
+    elif request.method == "GET":
+        form = MessageForm()  # unbound form
+
+        return render(
+            request,
+            'message_app/contact2.html',
+            context={
+                'form': form,
+            }
+        )
