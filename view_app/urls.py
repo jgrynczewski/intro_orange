a46729c2e08df7cadd0898aec8d4f4789dba8bae
jgrynczewski <jgrynczewski@gmail.com>
2023-05-13 12:01:59 +0200

    class view part 1

--- view_app/urls.py
+++ view_app/urls.py
@@ -6,4 +6,8 @@ app_name = 'view_app'
 
 urlpatterns = [
     path('hello/', views.hello, name='hello'),
+    path('hello2/', views.HelloView.as_view(), name='hello2'),
+
+    path('hello_template/', views.hello_function_view, name='hello_template'),
+    path('hello_template2/', views.HelloClassView.as_view(), name='hello_template2'),
 ]
--- view_app/views.py
+++ view_app/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render, HttpResponse
 
+from django import views
 
 # function view
 def hello(request):
     return HttpResponse("Hello, world!")
 
+
+# class view
+class HelloView(views.View):
+    def get(self, request):
+        return HttpResponse("Hello, world!")
+
+
+# function view
+def hello_function_view(request):
+    return render(
+        request,
+        'view_app/hello.html'
+    )
+
+
+# class view
+class HelloClassView(views.View):
+    def get(self, request):
+        return render(
+            request,
+            'view_app/hello.html'
+        )
