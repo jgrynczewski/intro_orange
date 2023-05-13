f064ca82d9870b1eeb259c73ac5f38a18e6c9289
jgrynczewski <jgrynczewski@gmail.com>
2023-05-13 11:39:59 +0200

    intermediate model in m2m

--- relations_app/models.py
+++ relations_app/models.py
@@ -31,3 +31,24 @@ class Actor(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     actors = models.ManyToManyField('Actor')  # i to też jest menadżer powiązany
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Band(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+
+    def __str__(self):
+        return self.name
+
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    band = models.ForeignKey(Band, on_delete=models.CASCADE)
+    date_joined = models.DateField()
