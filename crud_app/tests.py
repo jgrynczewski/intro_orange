70d35e1c3cbfa9b8c88cfb39752e09e745af8673
jgrynczewski <jgrynczewski@gmail.com>
2023-06-07 21:03:29 +0200

    tests in django

--- crud_app/tests.py
+++ crud_app/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse, resolve
+
+from crud_app.views import task_detail_view
+from crud_app.models import Task
+
 
 # Create your tests here.
+class TestUrls(SimpleTestCase):
+    def test_task_detail_view_url(self):
+        """Test sparametryzowanego url."""
+
+        url = reverse('crud_app:task_detail_view', args=[3])
+        view = resolve(url).func
+
+        self.assertEqual(view, task_detail_view)
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        """Ta metoda zostanie uruchomiona przed każdym testem w tej klasie."""
+
+        self.task = Task.objects.create(name='Pisanie')
+        self.task_list_view_url = reverse('crud_app:task_list_view')
+        self.task_detail_view_url = reverse('crud_app:task_detail_view', args=[1])
+
+    def test_task_list_view(self):
+        response = self.client.get(self.task_list_view_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'crud_app/task_list.html')
+
+        content = str(response.content)
+        self.assertIn("Lista", content)
+
+    def test_task_detail_view(self):
+        response = self.client.get(self.task_detail_view_url)
+
+        # Testowanie statusu odpowiedzi
+        self.assertEqual(response.status_code, 200)
+
+        # Testowanie wyświetlanego szablonu
+        self.assertTemplateUsed(response, 'crud_app/task_detail.html')
+
+        context = response.context
+        task = context.get('task')
+
+        # Testowanie szablonu wyświetlanego szablonu
+        self.assertIsInstance(task, Task)
+        self.assertEqual(task.name, 'Pisanie')
--- view_app/tests.py
+++ view_app/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
 
+from view_app.views import hello_function_view, HelloClassView
+
+# Testowanie w Django:
+# - testować urls
+# - testować widoku
+# - testować modele
+# - testować formularze
+
+
+# Testowanie urls
 # Create your tests here.
+class TestUrls(SimpleTestCase):
+
+    def test_hello_template_url(self):
+        """Testowanie url dla widoku funkcyjnego."""
+
+        url = reverse('view_app:hello_template')
+        # print(resolve(url))  # zawiera func, url_name, app_name, namespace, route, args i kwargs
+
+        view = resolve(url).func
+        # assert view == hello_function_view
+        self.assertEqual(view, hello_function_view)
+
+    def test_hello_template2_url(self):
+        """Testowanie url dla widoku klasowego."""
+
+        url = reverse('view_app:hello_template2')
+        view = resolve(url).func.view_class
+
+        self.assertEqual(view, HelloClassView)
